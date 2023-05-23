f9cc25546b0d5a33b64c5907d5803dd06c017f78
Youval Akouka <youvalakouka@gmail.com>
2023-05-23 16:50:05 +0200

    Rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating 10 fake articles...'
+10.times do
+  Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.sentence
+  ).save!
+end
+puts 'Finished!'
